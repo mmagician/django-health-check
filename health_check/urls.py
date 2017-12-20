73c01330439f6f1a53e5c7ece11cc8ef3cc20250
Marcin Gorny <marcin.gorny@knoxmediahub.com>
2017-12-20 10:30:55 +0100

    Generic urls Need to have a custom urls.py file in your app / add it to main urls.py

--- health_check/urls.py
+++ health_check/urls.py
@@ -5,6 +5,4 @@ from health_check.views import MainView
 
 urlpatterns = patterns('',
     url(r'^$', MainView.as_view(), name='health_check_home'),
-    url(r'^celery_remote$', MainView.as_view(registry='_registryremote'), name='celery_remote'),
-    url(r'^celery_default$', MainView.as_view(registry='_registrydefault'), name='celery_remote'),
 )
