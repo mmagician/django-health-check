bb776b496e1002bb7317d6437056653fd992d4c2
Marcin Gorny <marcin.gorny@knoxmediahub.com>
2017-12-21 17:25:04 +0100

    Default celery queue should be 'celery'

--- health_check/contrib/celery/apps.py
+++ health_check/contrib/celery/apps.py
@@ -22,10 +22,5 @@ class HealthCheckConfig(AppConfig):
             # Allow specifying the registry name
             registry_name = '_registry%s' % queue
 
-            # Apply_async will timeout if we pass it a 'default' queue
-            # Rather, it should take NoneType
-            if queue == 'default':
-                queue = None
-
             celery_class = type(celery_class_name, (CeleryHealthCheck,), {'queue_display_name': name, 'queue': queue})
             plugin_dir.register(celery_class, registry=registry_name)
